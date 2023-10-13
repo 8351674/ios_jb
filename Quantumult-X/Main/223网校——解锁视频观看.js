7e88b6514ad1b2d9419fbc6c8a532b7d2376d3ef
8351674 <117062997+8351674@users.noreply.github.com>
2023-10-14 06:04:12 +0800

    Create 223网校——解锁视频观看.js

---
+++ Quantumult-X/Main/223网校——解锁视频观看.js
@@ -0,0 +1,26 @@
+
+/******************************
+
+脚本名称: 223网校——解锁视频观看
+下载地址：https://is.gd/63j7aM
+软件版本：3.7.1
+脚本作者：✎执笔画浮尘﹏
+更新时间：2023-8-18
+QQ交流群：2278301
+TG交流群：https://t.me/ios_jb
+TG频道群：https://t.me/ios_apple_jb
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+[rewrite_local]
+
+^http[s]?:\/\/api.233.com\/v1\/course\/myclass\/myclassdetail url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/233.js
+
+[mitm] 
+
+hostname = api.233.com
+
+*******************************/
+
+var body = $response.body.replace(/freelistenTf":\d/g,'freelistenTf":1')
+$done({ body });
